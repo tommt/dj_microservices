98b6359fdec6d201bb4ab1482e92a4661162f31f
tommt <atombombmount@gmail.com>
2021-05-26 06:33:51 +0545

    products CRUD

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer
 from rest_framework.response import Response
 from rest_framework import status
+
+import random
 
 
 class ProductViewSet(viewsets.ViewSet):
@@ -13,13 +16,31 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
-        pass
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
-        pass
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(
+            instance=product, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk=None):
-        pass
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response({'success': 'removed products'}, status=status.HTTP_204_NO_CONTENT)
+
+
+def UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
